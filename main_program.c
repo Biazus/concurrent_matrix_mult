eeae97d24800d5d722a6dae2fec109a5a7b2235e
Biazus <miller_br@hotmail.com>
2015-03-25 00:34:08 -0300

    Running thread

--- main_program.c
+++ main_program.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int g;
 
 void *calculate(void *arg) {
     int i, n = *(int *) arg;
-    for(i = 0; i < n; i++)
+    for(i = 0; i < n; i++){
         g = i;
+            sleep(30);
+        printf("over\n");
+    }
 }
 
-void *do_it_2(void *arg) {
+/*void *do_it_2(void *arg) {
     int i, n = *(int *) arg;
     for(i = 0; i < n; i++)
-        printf("%d\n", g);
+        printf("%d\nhaushe", g);
 }
-
+*/
 int main( int argc, char **argv) {
-    pthread_t th1, th2;
+//    pthread_t th1, th2;
     pthread_t *tid;
     int n = 10;
-    int nthr = 0, i=0; //numero de threads a serem criadas (param)
+    int nthr = 1, i=0; //numero de threads a serem criadas (param)
+    tid = (pthread_t *)malloc(sizeof(pthread_t)*nthr);
+    for (i = 0; i < nthr; i++){
+       pthread_create(&tid[i], NULL,(void*) calculate, &n);
+      }
     for (i = 0; i < nthr; i++)
-       pthread_create(&tid[i], NULL, calculate, &n);
-
-//    pthread_create(&th1, NULL, do_it_1, &n);
-//    pthread_create(&th2, NULL, do_it_2, &n);
+       pthread_join(tid[i], NULL);
 	
 	printf("acabando...\n");
 }
