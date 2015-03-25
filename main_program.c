f84b9b3538c48ccf67c8a04808ecd4c4aeca1b68
Biazus <miller_br@hotmail.com>
2015-03-25 13:25:47 -0300

    starting to read

--- main_program.c
+++ main_program.c
@@ -5,26 +5,36 @@
 
 int g;
 
+int convert_file_to_c(){
+    FILE *input;
+    char *mode = "r";
+    input=fopen("teste.txt", mode);
+    if (input==NULL){
+	printf("Erro ao ler arquivo\n");
+        return 0;
+    }
+    return 1;
+
+}
+
 void *calculate(void *arg) {
     int i, n = *(int *) arg;
     for(i = 0; i < n; i++){
         g = i;
-            sleep(30);
         printf("over\n");
     }
 }
 
-/*void *do_it_2(void *arg) {
-    int i, n = *(int *) arg;
-    for(i = 0; i < n; i++)
-        printf("%d\nhaushe", g);
-}
-*/
 int main( int argc, char **argv) {
 //    pthread_t th1, th2;
     pthread_t *tid;
     int n = 10;
     int nthr = 1, i=0; //numero de threads a serem criadas (param)
+ 
+    if (!convert_file_to_c()){
+	return 0;
+    }
+    
     tid = (pthread_t *)malloc(sizeof(pthread_t)*nthr);
     for (i = 0; i < nthr; i++){
        pthread_create(&tid[i], NULL,(void*) calculate, &n);
